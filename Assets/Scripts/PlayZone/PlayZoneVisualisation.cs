253f2ffd611fe8441bd129891a69359686c3980d
Rodion <workavast@yandex.ru>
2024-06-05 19:57:36 +0300

    bug fix

--- Assets/Scripts/PlayZone/PlayZoneVisualisation.cs
+++ Assets/Scripts/PlayZone/PlayZoneVisualisation.cs
@@ -5,18 +5,21 @@ namespace SourceCode.PlayZone
 {
     public class PlayZoneVisualisation : MonoBehaviour
     {
-        private PlayZoneConfig _config;
+        [SerializeField] private PlayZoneConfig config;
 
         [Inject]
         public void Construct(PlayZoneConfig config)
         {
-            _config = config;
+            this.config = config;
         }
         
         private void OnDrawGizmos()
         {
+            if(config == null)
+                return;
+            
             Gizmos.color = Color.white;
-            Gizmos.DrawWireSphere(Vector2.zero, _config.Radius);
+            Gizmos.DrawWireSphere(Vector2.zero, config.Radius);
         }
     }
 }
